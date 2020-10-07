98e0db11010deb21a1e74bc0f3dd4ad66c669bda
CharlesWilmart <chwilmart@gmail.com>
2020-10-07 19:14:11 +0200

    Added routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //  importing all the stuff
 import express from "express";
 import mongoose from "mongoose";
+import dbMessages from "./dbMessages";
 
 //  app config
 const app = express();
@@ -20,5 +21,15 @@ mongoose.connect("connection_url", {
 
 // api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
+app.post("/api/v1/messages/new", (req, res) => {
+  const dbMessage = req.body;
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`new message created: \n ${data}`);
+    }
+  });
+});
 // listener
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
