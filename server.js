dfbbc783c38e1fd86d2abf329037393ae89d00eb
CharlesWilmart <chwilmart@gmail.com>
2020-10-10 20:42:49 +0200

    Added middleware

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ const pusher = new Pusher({
 });
 //  middleware
 app.use(express.json());
+app.use((req, res, next)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+})
 
 //  DB config
 const connection_url =
