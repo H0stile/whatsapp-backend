25926cdc1a5eaee10c209d744791a35c480d1a43
CharlesWilmart <chwilmart@gmail.com>
2020-10-12 21:50:28 +0200

    Added cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
 import Pusher from "pusher";
+import cors from "cors";
 //  const Pusher = require("pusher");
 
 //  app config
@@ -18,11 +19,13 @@ const pusher = new Pusher({
 });
 //  middleware
 app.use(express.json());
-app.use((req, res, next)=>{
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
-})
+// app.use((req, res, next)=>{
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   next();
+// })
+// No need if we use cors
+app.use(cors());
 
 //  DB config
 const connection_url =
